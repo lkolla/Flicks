2341f19c6e5578ea779a1454cc69f9259b650239
lkolla <lkolla@walmartlabs.com>
2016-07-17 22:15:19 -0700

    first commit

---
+++ Flicks/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  Flicks
+//
+//  Created by Rajesh Kolla on 7/17/16.
+//  Copyright © 2016 walmart. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
